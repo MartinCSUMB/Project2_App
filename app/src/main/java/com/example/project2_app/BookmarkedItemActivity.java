3752fa8858051b1b169654b84198126295914cd5
Krithika <krithikakyetchina@gmail.com>
2024-12-12 22:09:50 -0800

    ok it works if there are no bookmarked items

--- app/src/main/java/com/example/project2_app/BookmarkedItemActivity.java
+++ app/src/main/java/com/example/project2_app/BookmarkedItemActivity.java
@@ -45,7 +45,7 @@ public class BookmarkedItemActivity extends AppCompatActivity {
     private void displayBookmarkedItems(List<Product> bookmarkedProducts) {
         StringBuilder sb = new StringBuilder();
 
-        //need to test this out
+        //ok it works
         if (bookmarkedProducts.isEmpty()) {
             sb.append("No bookmarked items.");
         } else {
